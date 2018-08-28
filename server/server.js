c108e7ab8843e6fa704fdb273ba76e5e9aaa3433
ShriSurve <shriniwas.s@mobinexttech.com>
2018-08-29 00:54:54 +0530

    Added validation on mongoose model

--- server/server.js
+++ server/server.js
@@ -5,28 +5,49 @@ mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
 
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    required: true,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
 // var newTodo = new Todo({
-//   text: 'cooked dinner'
+//   text: ' cook dinner'
+// });
+//
+// newTodo.save().then((doc)=> {
+//   console.log('saved', JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('unable to save todo', e);
 // });
 
-var newTodo = new Todo({
-  text: 'read book',
-  completed: 'ghj',
-  completedAt: 10
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
+})
+
+
+var user = new User({
+  email: ' shriniwas@example.com'
 });
 
-newTodo.save().then((doc)=> {
-  console.log('saved', JSON.stringify(doc, undefined, 2));
+user.save().then((doc) => {
+  console.log('User saved', doc);
 }, (e) => {
-  console.log('unable to save todo', e);
+  console.log('Unable to save user', e);
 });
