f2431f6cc61cb94d581473780f80a6a35bb077e5
shriniwassurve <shriniwas.surve@gmail.com>
2018-09-06 07:48:52 +0530

    Monogoose code refactored. Added expressjs and body-parser

--- server/server.js
+++ server/server.js
@@ -1,53 +1,13 @@
-var mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
-
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    required: true,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
-});
-
-// var newTodo = new Todo({
-//   text: ' cook dinner'
-// });
-//
-// newTodo.save().then((doc)=> {
-//   console.log('saved', JSON.stringify(doc, undefined, 2));
-// }, (e) => {
-//   console.log('unable to save todo', e);
-// });
+var express = require('express');
+var bodyParser = require('body-parser')
 
 
-var User = mongoose.model('User', {
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 1
-  }
-})
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
+var app = express();
 
-var user = new User({
-  email: ' shriniwas@example.com'
-});
-
-user.save().then((doc) => {
-  console.log('User saved', doc);
-}, (e) => {
-  console.log('Unable to save user', e);
+app.listen(3000, () => {
+  console.log('started on port 3000');
 });
