8147cc79219520ecbf4e247e038aa1a5f3ad884c
shriniwassurve <shriniwas.surve@gmail.com>
2018-09-30 00:15:02 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ var bodyParser = require('body-parser')
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
+
 
 var app = express();
 
@@ -29,6 +31,25 @@ app.get('/todos', (req,res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req,res) => {
+
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send('Not Found');
+  }
+
+  Todo.findById(id).then((todo)=>{
+    if(!todo) {
+      return res.status(404).send('Not Found');
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
 });
 
 app.listen(3000, () => {
