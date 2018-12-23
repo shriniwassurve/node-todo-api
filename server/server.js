f351505d518f63086de1636cd67bae44743f31f6
shriniwassurve <shriniwas.surve@gmail.com>
2018-12-24 00:49:08 +0530

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const _ = require('lodash');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate')
 
 const {ObjectID} = require('mongodb');
 
@@ -106,6 +107,8 @@ app.patch('/todos/:id', (req, res) => {
 
 //add User
 app.post('/users', (req,res) => {
+  console.log('here1');
+
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
@@ -119,6 +122,10 @@ app.post('/users', (req,res) => {
     console.log(err);
     res.status(400).send(err);
   });
+});
+
+app.get('/users/me', authenticate, (req,res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
