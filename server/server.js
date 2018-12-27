f28033c9c6115b7384c3d7a078ac2dba34f6af99
shriniwassurve <shriniwas.surve@gmail.com>
2018-12-27 22:30:06 +0530

    Added DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -79,6 +79,7 @@ app.delete('/todos/:id', (req,res) => {
   });
 });
 
+// Update todo
 app.patch('/todos/:id', (req, res) => {
   var id = req.params.id;
   var body = _.pick(req.body, ['text', 'completed']);
@@ -122,10 +123,12 @@ app.post('/users', (req,res) => {
   });
 });
 
+// authenticate user
 app.get('/users/me', authenticate, (req,res) => {
   res.send(req.user);
 });
 
+// User login
 app.post('/users/login', (req,res) => {
   var body = _.pick(req.body, ['email', 'password']);
 
@@ -137,6 +140,16 @@ app.post('/users/login', (req,res) => {
   })
   .catch((err) => {
     res.status(400).send(err);
+  });
+});
+
+// User logout
+app.delete('/users/me/token', authenticate, (req,res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  })
+  .catch(() => {
+    res.status(400).send();
   });
 });
 
