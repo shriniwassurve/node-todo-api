166261dfd9e355be4dcbd08746a891026a1adece
shriniwassurve <shriniwas.surve@gmail.com>
2018-12-27 22:44:52 +0530

    Added test for DELETE /users/me/token route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -306,3 +306,25 @@ describe('POST /users/login', (done) => {
   });
 
 });
+
+
+describe('DELETE /users/me/token', (done) => {
+  it('should remove auth token from logout', (done) => {
+
+    request(app)
+    .delete('/users/me/token')
+    .set('x-auth', users[0].tokens[0].token)
+    .expect(200)
+    .end((err) => {
+      if (err) {
+        return done(err);
+      }
+
+      User.findById(users[0]._id).then((user) => {
+        expect(user.tokens.length).toBe(0);
+        done();
+      })
+      .catch(done);
+    });
+  })
+});
