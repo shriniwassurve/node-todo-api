97022ea8d9372665fa5c17e3cda0daa196aea862
shriniwassurve <shriniwas.surve@gmail.com>
2018-12-30 22:13:02 +0530

    update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,13 @@
 var env = process.env.NODE_ENV || 'development';
 
+if (env ==='development' ||env ==='test') {
+  var  config  = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
+}
+
 // changed localhost to 127.0.0.1 in order  to reduce lookup time caused for localhost to be chaged to 127.0.0.1
 // Read more: https://hackernoon.com/how-changing-localhost-to-127-0-0-1-sped-up-my-test-suite-by-1-800-8143ce770736
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGOLAB_URI = 'mongodb://127.0.0.1:27017/TodoApp';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGOLAB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
-}
--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -28,7 +28,7 @@ const users = [
     password: 'abcd123',
     tokens: [{
       access: 'auth',
-      token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
+      token: jwt.sign({_id: userOneId, access: 'auth'}, process.env.JWT_SECRET).toString()
     }]
   }, {
     _id: userTwoId,
@@ -36,7 +36,7 @@ const users = [
     password: 'abcd123',
     tokens: [{
       access: 'auth',
-      token: jwt.sign({_id: userTwoId, access: 'auth'}, 'abc123').toString()
+      token: jwt.sign({_id: userTwoId, access: 'auth'}, process.env.JWT_SECRET).toString()
     }]
   }
 ]
